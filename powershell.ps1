aaf101e34e0dec3588467e9eee23947892b3407a
Hotetzry <ryanhotetz@gmail.com>
2024-08-01 13:52:58 -0400

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -381,3 +381,142 @@ Function Get-Netinfo {
     
 '1.8.2.44'.split(".")[1]
 #1,2,3,4,5 -join ""
+
+
+
+
+Practice Exam #1 
+
+<# 1 #>
+function q1($var1,$var2,$var3,$var4) {
+    <# Return the product of the arguments #>
+    $prod = $var1 * $var2 * $var3 * $var4 
+    write-output "$prod"
+
+
+
+
+<# 2 #>
+function q2($arr,$rows,$cols,$key) {
+    <# Search the 2 dimensional array for the first occurance of key at column index 0
+       and return the value at column index 9 of the same row.
+       Return -1 if the key is not found.
+      
+    #>
+    foreach($row in $arr) {
+        if($row[0] -eq $key){
+            return $row[9]
+        }
+    }
+    return -1 
+}
+
+
+
+<# 3 #>
+
+function q3 {
+    <# In a loop, prompt the user to enter positive integers one at time.
+       Stop when the user enters a -1. Return the maximum positive
+       value that was entered."
+	#> $vals = @()
+       do { 
+       $val = read-host
+       if($val -ne -1){
+           $vals += $val
+    }
+    } until($val -eq -1)
+    return ($vals | measure-object -maximum).maximum
+ }
+
+
+
+
+<# 4 #>
+<# Return the line of text from the file given by the `$filename
+argument that corresponds to the line number given by `$whichline.
+The first line in the file corresponds to line number 0."
+#> 
+  function q4($filename,$whichline) {
+    $number = $whichline
+    $Answer = Get-Content $filename | Select-Object -index $number / $whichline  <---- ($whichline)
+    $Answer
+    
+}
+
+alternate 4
+$content = get-content $filename
+return $content[$whichline]
+return (get-content $filename)[$whichline]
+
+
+<# 5 #> 
+<# Return the child items from the given path sorted
+ascending by their Name #> 
+function q5($path) {
+       Get-childitem -path $path | sort-object name
+}
+
+
+<# 6 #>
+function q6 {
+    <# Return the sum of all elements provided on the pipeline
+	#>
+     $sum = 0
+     $input | foreach {$sum += $_ }
+     $sum
+     
+}
+
+
+<# 7 #>
+function q7 {
+	<# Return only those commands whose noun is process #>
+    get-command -noun 'process'
+}
+
+<# 8 #>
+function q8($adjective) {
+    <# Return the string 'PowerShell is ' followed by the adjective given
+	   by the `$adjective argument
+	#>    
+     -join ("PowerShell is", " $adjective")
+}
+
+
+<# 9 #> 
+function q9($addr) {
+	<# Return `$true when the given argument is a valid IPv4 address,
+	   otherwise return `$false. For the purpose of this function, regard
+	   addresses where all octets are in the range 0-255 inclusive to
+	   be valid.
+	#>
+    $pattern = '^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$'
+    if ($addr -match $pattern) { 
+        return $true
+        }
+    else{
+        return $false
+        }
+    
+}
+alternate 9 
+
+$pattern = '^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$'
+    return $addr -match $pattern  
+
+    
+<# 10 #>
+
+function q10 ($filepath,$lasthash) {
+    <# Return `$true if the contents of the file given in the
+       `$filepath argument have changed since `$lasthash was
+       computed. `$lasthash is the previously computed SHA256
+       hash (as a string) of the contents of the file. #> 
+       $A = get-filehash -path $filepath -Algorithm SHA256
+  
+       $NewHash = $A.hash 
+        return $Newhash -ne $lasthash
+      
+}
+
